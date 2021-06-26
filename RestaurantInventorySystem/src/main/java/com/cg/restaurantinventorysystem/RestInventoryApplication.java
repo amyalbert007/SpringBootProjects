e013fd65b06e1c909df06594926a86de9d9da0e5
amalbert007 <amyalbert007@gmail.com>
2021-06-26 22:24:49 +0530

    simple spring boot with HTML example commit

---
+++ RestaurantInventorySystem/src/main/java/com/cg/restaurantinventorysystem/RestInventoryApplication.java
@@ -0,0 +1,16 @@
+package com.cg.restaurantinventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class RestInventoryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestInventoryApplication.class, args);
+	}
+
+}
